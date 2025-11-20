77e34101becd4479bac2595e8b7046fcc534aa9f
a-zhenya <eadishchev@gmail.com>
2025-11-20 17:58:55 +0100

    Revert "Add sub_of_two function (#2)" (#3)
    This reverts commit 456d2144f7f04c872056070bd801f2b36da71199.

--- src/eadishchev_new/main.py
+++ src/eadishchev_new/main.py
@@ -1,6 +1,2 @@
 def sum_of_two(x: int, y: int) -> int:
-    return x + y
-
-
-def sub_of_two(x: int, y: int) -> int:
     return x - y
--- tests/test_subtraction.py
+++
@@ -1,9 +0,0 @@
-from eadishchev_new import sub_of_two
-
-
-def test_sum_one_plus_one():
-    assert sub_of_two(1, 1) == 0
-
-
-def test_sum_two_plus_two():
-    assert sub_of_two(5, 2) == 3
